a2078038a6b319246c02f3d67ca789f40fe86716
Herb Susmann <herbps10@gmail.com>
2024-04-16 11:59:05 -0400

    Added cross-fitting

--- R/riesz_superlearner.R
+++ R/riesz_superlearner.R
@@ -1,14 +1,19 @@
-riesz_superlearner_weights <- function(learners, task) {
-  risks <- lapply(learners, function(x) {
-    x$loss(task)
-  })
+riesz_superlearner_weights <- function(learners, task, folds) {
+  resampling <- mlr3::rsmp("cv", folds = folds)
+  resampling$instantiate(task)
 
-  unlist(risks)
+  cv_risks <- matrix(nrow = folds, ncol = length(learners))
+  for(fold in 1:folds) {
+    cv_learners <- lapply(learners, \(x) x$clone()$train(task$clone()$filter(resampling$train_set(fold))))
+    cv_risks[fold, ] <- unlist(lapply(cv_learners, \(x) x$loss(task)))
+  }
+
+  colMeans(cv_risks)
 }
 
 #' @import mlr3
 #' @export
-super_riesz <- function(data, data_shifted, library, conditional_indicator = matrix(ncol = 1, rep(1, nrow(data))), m = \(data, data_shifted, conditional_indicator, conditional_mean) data_shifted, discrete = TRUE) {
+super_riesz <- function(data, data_shifted, library, conditional_indicator = matrix(ncol = 1, rep(1, nrow(data))), m = \(data, data_shifted, conditional_indicator, conditional_mean) data_shifted, folds = 5, discrete = TRUE) {
   task <- TaskRiesz$new(id = "superriesz", data, data_shifted, conditional_indicator, m)
 
   if(is.list(library)) {
@@ -24,16 +29,23 @@ super_riesz <- function(data, data_shifted, library, conditional_indicator = mat
     })
   }
 
+  if(folds > 1) {
+    cv_risks <- riesz_superlearner_weights(learners, task, folds)
+  }
+
   # Train learners
   lapply(learners, \(learner) learner$train(task))
-  risks <- riesz_superlearner_weights(learners, task)
+
+  if(folds == 1) {
+    cv_risks <- unlist(lapply(learners, \(learner) learner$loss(task)))
+  }
 
   if(length(library) == 1 || discrete) {
     weights <- numeric(length(library))
-    weights[which.min(risks)] <- 1
+    weights[which.min(cv_risks)] <- 1
   }
 
-  sl <- list(learners = learners, weights = weights, m = m, risk = risks)
+  sl <- list(learners = learners, weights = weights, m = m, risk = cv_risks)
   class(sl) <- "super_riesz"
   sl
 }
