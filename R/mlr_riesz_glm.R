1bb82e78e235b241008b17663fe7f7a7fe0e5999
Herb Susmann <herbps10@gmail.com>
2024-08-23 17:06:13 -0400

    Updated GLM optim based learner

--- R/mlr_riesz_glm.R
+++ R/mlr_riesz_glm.R
@@ -1,6 +1,7 @@
 #' @importFrom stats optim
 glm_estimate_representer <- function(data,
   m,
+  lambda = 0,
   constrain_positive = TRUE) {
   if(constrain_positive == TRUE) {
     transform <- exp
@@ -10,19 +11,25 @@ glm_estimate_representer <- function(data,
   }
 
   pred <- function(beta, x) {
-    transform(as.matrix(cbind(1, x)) %*% beta)
+    transform(as.matrix(x) %*% beta)
   }
 
   loss <- function(beta) {
     alpha <- function(x) pred(beta, x)
     y <- m(alpha, data)
     if(is.data.frame(y) || is.data.table(y)) y <- y[[1]]
-    mean(alpha(data())^2 - 2 * y)
+    (mean((alpha(data())^2 - 2 * y)[, 1]) + lambda * mean(beta^2))
   }
 
-  pars <- numeric(ncol(data()) + 1)
-  x <- optim(pars, fn = loss)
+  pars <- numeric(ncol(data()))
+  x1 <- optim(pars, fn = loss)
+  x2 <- optim(pars, fn = loss, method = "BFGS")
+
+  x <- x1
+  if(x2$value < x1$value && x2$convergence == 0) x <- x2
+
   beta <- x$par
+  print(beta)
   beta
 }
 
@@ -35,7 +42,9 @@ LearnerRieszGLM <- R6::R6Class(
     #' @importFrom paradox ps p_lgl
     initialize = function() {
       params <- ps(
-        constrain_positive = p_lgl(default = TRUE, tags = "train")
+        constrain_positive = p_lgl(default = TRUE, tags = "train"),
+        lambda = p_dbl(default = 0, tags = "train"),
+        interactions = p_int(1L, default = 1L, tags = "data")
       )
 
       super$initialize(
@@ -47,21 +56,39 @@ LearnerRieszGLM <- R6::R6Class(
       )
     },
     loss = function(task) {
+      data <- private$.glm_data(task)
       mean(
-        self$alpha(task$data())^2 - 2 * task$m(self$alpha, task$data)[[1]]
+        self$alpha(data())^2 - 2 * task$m(self$alpha, data)[[1]]
       )
     },
     alpha = function(x) {
-      pv <- self$param_set$get_values(tags = "train")
+      pv <- self$param_set$get_values(tags = c("train", "data"))
       constrain_positive <- TRUE
       if(!is.null(pv$constrain_positive)) constrain_positive <- pv$constrain_positive
       transform <- \(x) x
       if(constrain_positive == TRUE) transform <- exp
-      transform(as.matrix(cbind(1, x)) %*% self$model)
+      transform(as.matrix(x) %*% self$model)
     }
   ),
   private = list(
     .model = NULL,
+
+    .glm_data = function(task) {
+      # Convert natural data to torch tensor
+      pv <- self$param_set$get_values(tags = "data")
+
+      data <- add_interactions(task$data(), pv$interactions)
+
+      # Convert all alternative versions of the data
+      alternatives = lapply(names(task$alternatives), \(x) add_interactions(task$data(x), pv$interactions))
+      names(alternatives) <- names(task$alternatives)
+
+      function(key = NA) {
+        if(is.na(key)) return(data)
+        if(key %in% names(task$extra)) return(task$data(key))
+        return(alternatives[[key]])
+      }
+    },
 
     .train = function(task) {
       pv <- self$param_set$get_values(tags = "train")
@@ -69,13 +96,13 @@ LearnerRieszGLM <- R6::R6Class(
       self$model <-
         mlr3misc::invoke(
           glm_estimate_representer,
-          data = task$data,
+          data = private$.glm_data(task),
           m = task$m,
           .args = pv
         )
     },
     .predict = function(task) {
-      response <- self$alpha(task$data())
+      response <- self$alpha(private$.glm_data(task)())
       list(response = response)
     }
   )
