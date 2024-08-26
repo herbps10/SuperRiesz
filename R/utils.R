d3bf2d141f5def1156288189e5a054ceec11f6b0
Herb Susmann <herbps10@gmail.com>
2024-08-26 18:56:24 -0400

    Bugfix in generating interactions

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@
   interactions <- min(interactions, ncol(df) - 1)
   if(interactions > 1) {
     for(j in 2:interactions) {
-      vars <- t(combn(names(df), j))
+      vars <- t(combn(colnames(df), j))
 
       for(i in 1:nrow(vars)) {
         var <- vars[i, ]
